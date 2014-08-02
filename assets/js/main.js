460e45786370a93646ee1efbfb32caf0ae347f33
Joe Nyland <joenyland@me.com>
2014-08-02 22:52:43 +0100

    Create site layout with dummy text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function(){
+    /* Activate ScrollSpy Menu */
+    $('body').scrollspy({
+        target: '#navbar-collapsible',
+        offset: 50
+    });
+
+    /* Smooth Scrolling */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 48     // (50 - 2), to make sure the nav gets updated correctly.
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
