9904d99ff47b465868bf00b58a0531e4c54516b0
Joe Nyland <joenyland@me.com>
2016-10-25 15:56:00 +0100

    Build Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 255, minimum: 3 }, uniqueness: {case_sensitive: false}
+  validates :website, presence: true
+
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :companies
+
+  before do
+    @company = companies(:company1)
+  end
+
+  context 'attributes' do
+
+    it 'should be valid' do
+      expect(@company).to be_valid
+    end
+
+    it 'should require a name' do
+      @company.name = ''
+      expect(@company).not_to be_valid
+      @company.name = nil
+      expect(@company).not_to be_valid
+    end
+
+    it 'name should not allow names that are too long' do
+      @company.name = 'a'*300
+      expect(@company).not_to be_valid
+    end
+
+    it 'name should not be too short' do
+      @company.name = 'a'*2
+      expect(@company).not_to be_valid
+    end
+
+    it 'should require a website' do
+      @company.website = ''
+      expect(@company).not_to be_valid
+      @company.website = nil
+      expect(@company).not_to be_valid
+    end
+
+  end
+
 end
